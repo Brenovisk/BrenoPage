8d3ab9c40cb3e9f8c03c2099ffba1a358a08ba96
Brenovisk <brenolucasdeos@gmail.com>
2021-02-02 17:09:25 -0300

    Scroll done

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
-
+import { Link, animateScroll as scroll } from 'react-scroll'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faUser,
@@ -27,8 +27,20 @@ export default function LabelBottomNavigation() {
   const classes = useStyles()
   const [value, setValue] = React.useState('about')
 
+  var Scroll = require('react-scroll')
+  var scroller = Scroll.scroller
+  var ScrollLink = Scroll.ScrollLink
+
   const handleChange = (event, newValue) => {
     setValue(newValue)
+
+    scroller.scrollTo(newValue, {
+      spy: true,
+      smooth: 'easeInOutQuint',
+      offset: -150,
+      duration: 1500,
+      hashSpy: 'one',
+    })
   }
 
   return (
@@ -43,6 +55,7 @@ export default function LabelBottomNavigation() {
         value="about"
         icon={<FontAwesomeIcon icon={faUser} />}
       />
+
       <BottomNavigationAction
         label="Projetos"
         value="projects"
@@ -53,6 +66,7 @@ export default function LabelBottomNavigation() {
         value="historic"
         icon={<FontAwesomeIcon icon={faHistory} />}
       />
+
       <BottomNavigationAction
         label="Contato"
         value="contact"
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,9 @@ const HomeIndex = () => {
   const siteTitle = 'BrenoLucas'
   const siteDescription = 'Site pessoal de Breno Lucas'
 
+  var Scroll = require('react-scroll')
+  var Element = Scroll.Element
+
   return (
     <Layout>
       <Helmet>
@@ -18,116 +21,138 @@ const HomeIndex = () => {
       <LabelBottomNavigation />
 
       <div id="main">
-        <section id="one">
-          <header className="major">
-            <h2>
-              Ipsum lorem dolor aliquam ante commodo
-              <br />
-              magna sed accumsan arcu neque.
-            </h2>
-          </header>
-          <p>
-            Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc
-            nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae
-            lobortis tortor primis integer massa adipiscing id nisi accumsan
-            pellentesque commodo blandit enim arcu non at amet id arcu magna.
-            Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate
-            lorem neque cubilia.
-          </p>
-          <ul className="actions">
-            <li>
-              <a href="#" className="button">
-                Learn More
-              </a>
-            </li>
-          </ul>
-        </section>
+        <Element name="about">
+          <section id="one">
+            <header className="major">
+              <h2>Um pouco sobre mim...</h2>
+            </header>
+            <p>
+              Sou Desenvolvedor Web FrontEnd fascinado por UI/UX design e novas
+              tecnologias. Com criatividade e empenho desenvolvo interfaces
+              funcionais e minimalistas com foco total na necessidade do
+              cliente.
+            </p>
 
-        <section id="two">
-          <h2>Recent Work</h2>
+            <b>Empresa atual: </b>
+          </section>
+        </Element>
 
-          <Gallery />
+        <Element name="projects">
+          <section id="two">
+            <header className="major">
+              <h2>Projetos</h2>
+            </header>
 
-          <ul className="actions">
-            <li>
-              <a href="#" className="button">
-                Full Portfolio
-              </a>
-            </li>
-          </ul>
-        </section>
+            <Gallery />
 
-        <section id="three">
-          <h2>Get In Touch</h2>
-          <p>
-            Accumsan pellentesque commodo blandit enim arcu non at amet id arcu
-            magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem
-            vulputate lorem neque lorem ipsum dolor.
-          </p>
-          <div className="row">
-            <div className="8u 12u$(small)">
-              <form method="post" action="#">
-                <div className="row uniform 50%">
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder="Name"
-                    />
+            <ul className="actions">
+              <li>
+                <a href="#" className="button">
+                  Full Portfolio
+                </a>
+              </li>
+            </ul>
+          </section>
+        </Element>
+
+        <Element name="historic">
+          <section id="one">
+            <header className="major">
+              <h2>Histórico</h2>
+            </header>
+            <p>
+              Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc
+              nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae
+              lobortis tortor primis integer massa adipiscing id nisi accumsan
+              pellentesque commodo blandit enim arcu non at amet id arcu magna.
+              Accumsan orci faucibus id eu lorem semper nunc nisi lorem
+              vulputate lorem neque cubilia.
+            </p>
+            <ul className="actions">
+              <li>
+                <a href="#" className="button">
+                  Learn More
+                </a>
+              </li>
+            </ul>
+          </section>
+        </Element>
+
+        <Element name="contact">
+          <section id="three">
+            <header className="major">
+              <h2>Contato</h2>
+            </header>
+            <p>
+              Accumsan pellentesque commodo blandit enim arcu non at amet id
+              arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi
+              lorem vulputate lorem neque lorem ipsum dolor.
+            </p>
+            <div className="row">
+              <div className="8u 12u$(small)">
+                <form method="post" action="#">
+                  <div className="row uniform 50%">
+                    <div className="6u 12u$(xsmall)">
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div className="6u 12u$(xsmall)">
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div className="12u">
+                      <textarea
+                        name="message"
+                        id="message"
+                        placeholder="Message"
+                        rows="4"
+                      ></textarea>
+                    </div>
                   </div>
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="12u">
-                    <textarea
-                      name="message"
-                      id="message"
-                      placeholder="Message"
-                      rows="4"
-                    ></textarea>
-                  </div>
-                </div>
-                <ul className="actions" style={{ marginTop: 30 }}>
+                  <ul className="actions" style={{ marginTop: 30 }}>
+                    <li>
+                      <input type="submit" value="Send Message" />
+                    </li>
+                  </ul>
+                </form>
+              </div>
+              <div className="4u 12u$(small)">
+                <ul className="labeled-icons">
                   <li>
-                    <input type="submit" value="Send Message" />
+                    <h3 className="icon fa-home">
+                      <span className="label">Address</span>
+                    </h3>
+                    1234 Somewhere Rd.
+                    <br />
+                    Nashville, TN 00000
+                    <br />
+                    United States
+                  </li>
+                  <li>
+                    <h3 className="icon fa-mobile">
+                      <span className="label">Phone</span>
+                    </h3>
+                    000-000-0000
+                  </li>
+                  <li>
+                    <h3 className="icon fa-envelope-o">
+                      <span className="label">Email</span>
+                    </h3>
+                    <a href="#">hello@untitled.tld</a>
                   </li>
                 </ul>
-              </form>
+              </div>
             </div>
-            <div className="4u 12u$(small)">
-              <ul className="labeled-icons">
-                <li>
-                  <h3 className="icon fa-home">
-                    <span className="label">Address</span>
-                  </h3>
-                  1234 Somewhere Rd.
-                  <br />
-                  Nashville, TN 00000
-                  <br />
-                  United States
-                </li>
-                <li>
-                  <h3 className="icon fa-mobile">
-                    <span className="label">Phone</span>
-                  </h3>
-                  000-000-0000
-                </li>
-                <li>
-                  <h3 className="icon fa-envelope-o">
-                    <span className="label">Email</span>
-                  </h3>
-                  <a href="#">hello@untitled.tld</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
+          </section>
+        </Element>
       </div>
     </Layout>
   )
