4533fac6f7f3f5f017820c893b0b88daa52fafc4
Brenovisk <brenolucasdeos@gmail.com>
2021-03-17 14:48:40 -0300

    Install Means Plugins

---
+++ config/metadata.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: 'Breno Lucas Developer',
+    author: 'Breno Lucas',
+    description: 'Um pequeno site sobre mim',
+    social: [
+      {
+        name:'github',
+        url: 'https://github.com/Brenovisk'
+      },
+      {
+        name:'instagram',
+        url:'https://www.instagram.com/brenovks/'
+      },
+      {
+        name:'facebook',
+        url:"https://www.facebook.com/brenovks/"
+      }
+    ]
+  }
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,84 @@
+const siteMetadata = require('./config/metadata.js');
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Strata by HTML5 UP',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js Starter based on Strata by HTML5 UP',
+    title: 'Breno Lucas Developer',
+    author: 'Breno Lucas',
+    description: 'Um pequeno site sobre mim',
+    siteUrl:  `https://www.brenolucas.com`,
+    social: [
+      {
+        name:'github',
+        url: 'https://github.com/Brenovisk'
+      },
+      {
+        name:'instagram',
+        url:'https://www.instagram.com/brenovks/'
+      },
+      {
+        name:'facebook',
+        url:"https://www.facebook.com/brenovks/"
+      }
+    ]
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.brenolucas.com`,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Breno Lucas Developer',
+        short_name: 'Breno Dev',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#504D4E',
+        theme_color: '#504D4E',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/logoB.png', // 512 x 512
+        
       },
     },
     'gatsby-plugin-sass',
